2df7f43295b47093ba4884dec0ef293adc03aa3b
Fahim2000 <fahim.193902022@green.ac.bd>
2024-09-23 22:27:44 +0600

    Feedbacks done

--- best-selling-product.js
+++ best-selling-product.js
@@ -1,30 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
   const tabButtons = document.querySelectorAll(".tab-buttons");
   const tabContents = document.querySelectorAll(".tab-contents");
-  // console.log(tabButtons)
+
   tabButtons.forEach((button) => {
     button.addEventListener("click", () => {
-      console.log("BUtton is clicked");
+      
 
       tabButtons.forEach((button) => {
         button.classList.remove("active");
-        console.log('dgfsfg')
+        
       });
 
       tabContents.forEach((content) => {
         content.classList.remove("tab-active");
-        console.log("here");
+        
       });
 
-      console.log(tabContents)
+      
 
       button.classList.add("active");
 
       const target = button.getAttribute("data-target");
 
       document.getElementById(target).classList.add("tab-active");
-      console.log(document.getElementById(target));
-      console.log(target);
+      
+      
     });
   });
   document.querySelector('.tab-buttons[data-target="slider1"]').click();
--- hamburger-menu.js
+++ hamburger-menu.js
@@ -1,46 +1,61 @@
 document.addEventListener("DOMContentLoaded", () => {
-
   const firstNavLinkMobile = document.querySelectorAll(".first-nav-link")[1];
   const firstNavLinkDesktop = document.querySelectorAll(".first-nav-link")[0];
   const submenu = document.querySelectorAll(".submenu")[1];
   const submenuDesktop = document.querySelectorAll(".submenu")[0];
   const hamburger = document.querySelector(".hamburger-menu");
-  const navMenu = document.querySelector(".nav-menu-mobile");
-  const caretImg = document.querySelectorAll(".caret-img")[1]
-  const caretImgDesktop = document.querySelectorAll(".caret-img")[0]
+  let navMenu = document.querySelector(".nav-menu-mobile");
+  const caretImg = document.querySelectorAll(".caret-img")[1];
+  const caretImgDesktop = document.querySelectorAll(".caret-img")[0];
+  let navMenuHeight = navMenu.offsetHeight + 8;
+  
+
+  console.log("hey", submenu.classList.contains('active-nav'))
   hamburger.addEventListener("click", () => {
-    console.log("hamburger clicked");
-      navMenu.classList.toggle("active-nav");
+    
+
+    console.log("nav menu height", navMenuHeight);
+
+    navMenu.classList.toggle("active-nav");
+
+    if (navMenu.classList.contains("active-nav")) {
+      navMenu.style.bottom = `-${navMenuHeight}px`;
+    } else {
+      navMenu.style.bottom = `200px`;
+    }
   });
 
-
   firstNavLinkMobile.addEventListener("click", (event) => {
+    const submenuMobileHeight = submenu.offsetHeight;
+    console.log("submenu mobile height", submenuMobileHeight);
     event.preventDefault();
-      console.log('firstNavLink clicked');
-      submenu.classList.toggle("active-nav"); 
+    // console.log("firstNavLink clicked");
+    submenu.classList.toggle("active-nav");
 
-   
     if (submenu.classList.contains("active-nav")) {
-      caretImg.style.transform = "rotate(180deg)"; 
-    } 
-  
-  else {
-    caretImg.style.transform = "rotate(0deg)"; 
-  }
+      caretImg.style.transform = "rotate(180deg)";
+      navMenuHeight = navMenu.offsetHeight + 8 + submenuMobileHeight;
+      navMenu.style.bottom = `-${navMenuHeight}px`;
+
+    } else {
+      caretImg.style.transform = "rotate(0deg)";
+      navMenuHeight = navMenu.offsetHeight + 8;
+      navMenu.style.bottom = `-${navMenuHeight}px`;
+
+      console.log('after colapse first nav',navMenuHeight)
+
+    }
   });
 
   firstNavLinkDesktop.addEventListener("click", (event) => {
     event.preventDefault();
-      console.log('firstNavLink clicked');
-      submenuDesktop.classList.toggle("active-nav"); 
+    // console.log("firstNavLink clicked");
+    submenuDesktop.classList.toggle("active-nav");
 
-   
     if (submenuDesktop.classList.contains("active-nav")) {
-      caretImgDesktop.style.transform = "rotate(180deg)"; 
-    } 
-  
-  else {
-    caretImgDesktop.style.transform = "rotate(0deg)"; 
-  }
+      caretImgDesktop.style.transform = "rotate(180deg)";
+    } else {
+      caretImgDesktop.style.transform = "rotate(0deg)";
+    }
   });
 });
